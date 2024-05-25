94982265f945659de4b1e9459ee9c0b76748c219
Sanaz.darbandi <mahta17rw@gmail.com>
2024-05-25 17:05:22 +0330

    1.add (class Address)

---
+++ Project4AP/Project4AP/Address.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef ADDRESS_H
+#define ADDRESS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Address {
+private:
+	string country;
+	string city;
+	string street;
+};
+
+#endif // ADDRESS_H
+
