5c8c6f0ece869c0b73012d0dde50725b60f7f278
Sanaz.darbandi <mahta17rw@gmail.com>
2024-05-25 17:19:52 +0330

    add operator << >>(Address)

--- Project4AP/Project4AP/Address.cpp
+++ Project4AP/Project4AP/Address.cpp
@@ -3,3 +3,16 @@
 // Constructor
 Address::Address(string country, string city, string street)
 	: country(country), city(city), street(street) {}
+
+
+// Overloading the << operator
+ostream& operator<<(ostream& os, const Address& address) {
+	os << "Country: " << address.country << ", City: " << address.city << ", Street: " << address.street;
+	return os;
+}
+
+// Overloading the >> operator
+istream& operator>>(istream& is, Address& address) {
+	is >> address.country >> address.city >> address.street;
+	return is;
+}
--- Project4AP/Project4AP/Address.h
+++ Project4AP/Project4AP/Address.h
@@ -15,6 +15,12 @@ private:
 public:
 	// Constructor
 	Address(string country, string city, string street);
+
+	// Overloading the << operator
+	friend ostream& operator<<(ostream& os, const Address& address);
+
+	// Overloading the >> operator
+	friend istream& operator>>(istream& is, Address& address);
 };
 
 #endif // ADDRESS_H
